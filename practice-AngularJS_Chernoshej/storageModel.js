373f0951531ed6bef203c2607064046d5da8cf12
Alexander Chernoshej <demoman361@gmail.com>
2015-08-05 11:06:11 +0300

    + add storage model and delete not relat4ed files from branch

---
+++ practice-AngularJS_Chernoshej/storageModel.js
@@ -0,0 +1,35 @@
+var storageModel = (function () {
+	return {
+		base : [
+            { name: "dough", amount: 10, minAmount: 1 }],
+        sauce : [
+            { name: "tomato", amount: 10, minAmount: 1 },
+			{ name: "smoked", amount: 10, minAmount: 1 },
+			{ name: "hot", amount: 10, minAmount: 1 }],
+        meat : [
+            { name: "bacon", amount: 10, minAmount: 1 },
+			{ name: "ham", amount: 10, minAmount: 1 },
+			{ name: "sausage", amount: 10, minAmount: 1 }],
+        vegetables : [
+            { name: "tomato", amount: 10, minAmount: 1 },
+			{ name: "champignon", amount: 10, minAmount: 1 },
+			{ name: "cucumber", amount: 10, minAmount: 1 },
+			{ name: "black olives", amount: 10, minAmount: 1 },
+			{ name: "olives", amount: 10, minAmount: 1 }],
+        seafood : [
+            { name: "shrimp", amount: 10, minAmount: 1 },
+			{ name: "squid", amount: 10, minAmount: 1 }],
+        cheese : [
+            { name: "mocarella", amount: 10, minAmount: 1 },
+			{ name: "bri", amount: 10, minAmount: 1 },
+			{ name: "feta", amount: 10, minAmount: 1 },
+			{ name: "cheder", amount: 10, minAmount: 1 },
+			{ name: "holland", amount: 10, minAmount: 1 }],
+        seasonigs : [
+            { name: "garlic", amount: 10, minAmount: 1 },
+			{ name: "lemon", amount: 10, minAmount: 1 },
+			{ name: "greenery", amount: 10, minAmount: 1 },
+			{ name: "chilli", amount: 10, minAmount: 1 },
+			{ name: "bulgarian pepper", amount: 10, minAmount: 1 }]
+	}
+})();
